564c33a6926824cdf194476fab5dc9a304efed75
Vinícius Varjão <moraesvxp@gmail.com>
2023-11-14 14:40:31 -0300

    little refactoring

--- src/assets/tailwindtheme.js
+++ src/assets/tailwindtheme.js
@@ -6,10 +6,17 @@ tailwind.config = {
                 {
                     DEFAULT: '#1d4ed8',
                     light: '#3b82f6',
+                    dark: '#1e3a8a',
                 },
-                secondary: '#eab308'
-            }
+                secondary: '#eab308',
+                paragraph: {
+                    DEFAULT: '#171717',
+                    light: '#404040',
+                }
+            },
+            fontFamily: {
+                'montserrat': ['Montserrat', 'sans-serif'],
+            },
         }
-
     }
 }
