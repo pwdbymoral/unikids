64f7e9601a303a52c9a76142b9bfbe0507db608e
Vinícius Varjão <moraesvxp@gmail.com>
2023-11-14 09:02:40 -0300

    updates

---
+++ src/assets/tailwindtheme.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary:
+                {
+                    DEFAULT: '#1d4ed8',
+                    light: '#3b82f6',
+                },
+                secondary: '#eab308'
+            }
+        }
+
+    }
+}
